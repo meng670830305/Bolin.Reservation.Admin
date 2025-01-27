1b8b1b358337738cd378ff4faea8bb293318912b
bolin <meng670830305@gmail.com>
2025-01-27 22:11:36 +0900

    添加项目文件。

---
+++ reservation/config/AutofacModuleRegister.cs
@@ -0,0 +1,21 @@
+﻿using Autofac;
+using System.Reflection;
+
+namespace webapi.config
+{
+    /// <summary>
+    /// Dependency Injection Container
+    /// 重写Autofac管道Load方法，在这里注册注入
+    /// </summary>
+    public class AutofacModuleRegister : Autofac.Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            //Interface Library Name
+            Assembly interfaceAssembly = Assembly.Load("Interface");
+            //Service Library Name
+            Assembly serviceAssembly = Assembly.Load("Service");
+            builder.RegisterAssemblyTypes(interfaceAssembly, serviceAssembly).AsImplementedInterfaces();
+        }
+    }
+}
