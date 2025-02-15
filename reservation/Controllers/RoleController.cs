a0782dbbc70f4ec313544762810c014a017edb4b
bolin <meng670830305@gmail.com>
2025-02-16 00:24:57 +0900

    add ToolController

---
+++ reservation/Controllers/RoleController.cs
@@ -0,0 +1,48 @@
+﻿using Interface;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Model.Dto.Role;
+using Model.Other;
+using webapi.Config;
+
+namespace webapi.Controllers
+{
+    public class RoleController : BaseController
+    {
+        private readonly IRoleService _roleService;
+        public RoleController(IRoleService roleService)
+        {
+            _roleService = roleService;
+        }
+
+        [HttpPost]
+        public async Task<ApiResult> Add(RoleAdd req)
+        {
+            userId = HttpContext.User.Claims.ToList()[0].Value;
+            return ResultHelper.Success(await _roleService.Add(req, userId));
+        }
+
+        [HttpPost]
+        public async Task<ApiResult> Edit(RoleEdit req)
+        {
+            userId = HttpContext.User.Claims.ToList()[0].Value;
+            return ResultHelper.Success(await _roleService.Edit(req, userId));
+        }
+        [HttpGet]
+        public async Task<ApiResult> Del(string id)
+        {
+            return ResultHelper.Success(await _roleService.Del(id));
+        }
+        [HttpGet]
+        public async Task<ApiResult> BatchDel(string ids)
+        {
+            return ResultHelper.Success(await _roleService.BatchDel(ids));
+        }
+        [HttpPost]
+        public async Task<ApiResult> GetRoles(RoleReq req)
+        {
+            userId = HttpContext.User.Claims.ToList()[0].Value;
+            return ResultHelper.Success(await _roleService.GetRoles(req, userId));
+        }
+    }
+}
